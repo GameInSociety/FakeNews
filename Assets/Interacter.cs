bd6512d473204d7fe21f0392fab99054f636c044
Gabylone <gameinsociety.dev@gmail.com>
2024-05-18 21:57:21 +0200

    player push

--- Assets/Interacter.cs
+++ Assets/Interacter.cs
@@ -15,6 +15,7 @@ public class Interacter : MonoBehaviour
 
     public GameObject reticle_group;
     public CanvasGroup reticle_cg;
+    bool selected = false;
 
     private void Awake()
     {
@@ -27,45 +28,11 @@ public class Interacter : MonoBehaviour
         HideReticleDelay();   
     }
 
-    bool inside = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (!holdingItem )
-        {
-            if (CameraBehavior.Instance.takingPhoto) {
-                if (inside)
-                    HideReticle();
-                return;
-
-            }
-            var ray = new Ray(transform.position, transform.forward);
-            var hit = new RaycastHit();
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-                var interactable = hit.collider.GetComponent<Interactable>();
-                if (interactable != null && interactable.able)
-                {
-                    if (!inside) {
-                        inside = true;
-                        ShowReticle();
-                    }
-                    if (Input.GetMouseButtonDown(0)) {
-                        PickUpItem(interactable.transform);
-                    }
-                } else {
-                    if (inside) {
-                        HideReticle();
-                        inside = false;
-                    }
-                }
-            } else if (inside) {
-                HideReticle();
-                inside = false;
-            }
-        } else
-        {
+        if (holdingItem) {
             currentItem.position = Vector3.Lerp(currentItem.position, target.position, speed * Time.deltaTime);
             currentItem.rotation = Quaternion.Lerp(currentItem.rotation, target.rotation, speed * Time.deltaTime);
 
@@ -74,8 +41,49 @@ public class Interacter : MonoBehaviour
                 currentItem.GetComponent<Rigidbody>().isKinematic = false;
                 currentItem.GetComponent<Rigidbody>().AddForce(transform.forward * force);
             }
+            return;
         }
+
+        if (CameraBehavior.Instance.takingPhoto) {
+            Deselect();
+            return;
+        }
+
+        var ray = new Ray(transform.position, transform.forward);
+        var hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit, 100f)) {
+            var interactable = hit.collider.GetComponent<Interactable>();
+            if (interactable != null && interactable.able) {
+                Select();
+                if (Input.GetMouseButtonDown(0))
+                    PickUpItem(interactable.transform);
+            } else {
+                Deselect();
+            }
+            return;
+        }
+        Deselect();
     }
+
+    void Select() {
+        if (selected)
+            return;
+        selected = true;
+        ShowReticle();
+        gameObject.layer = 7;
+        Debug.Log($"selcte");
+    }
+
+    void Deselect() {
+        if (!selected)
+            return;
+        selected = false;
+        HideReticle();
+        gameObject.layer = 0;
+        Debug.Log($"deselect");
+
+    }
+
     void ShowReticle() {
         reticle_group.SetActive(true);
         reticle_cg.alpha = 0f;
@@ -93,8 +101,7 @@ public class Interacter : MonoBehaviour
         reticle_group.SetActive(false);
     }
     public void PickUpItem(Transform interactable) {
-        HideReticle();
-        inside = false;
+        Deselect();
 
         holdingItem = true;
         currentItem = interactable.transform;
